cb0f7bc427284ee2b947c0bed3de19b9fab35eb6
GiancarloCP <giancarlocalderonp@gmail.com>
2021-01-08 14:35:26 -0500

    Zomato API y configuracion inicial.

--- app/src/main/java/com/platzi/ejerciciogian/ListadoActivity.kt
+++ app/src/main/java/com/platzi/ejerciciogian/ListadoActivity.kt
@@ -3,9 +3,9 @@ package com.platzi.ejerciciogian
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class ListadoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_listado)
     }
 }
